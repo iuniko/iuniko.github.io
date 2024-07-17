d09a16ae2f7c6f9d8dafa56dbbd8328a53700a07
iuniko <92965250+iuniko@users.noreply.github.com>
2024-07-17 17:22:15 +0800

    Add files via upload

---
+++ assets/start-Dy8X3VPD.js
@@ -0,0 +1,14 @@
+import{j as e,o as t,c as n,k as p}from"./index-B2OjM_1S.js";const s={class:"markdown-body"},r=p(`<h6>开发指南</h6><h1>快速上手</h1><p>跟随以下的步骤，快速上手组件库的使用。</p><hr><h3>安装</h3><pre>    <code class="">
+        npm install iuniko-ui
+    </code>
+</pre><h3>引用</h3><pre>    <code class="">
+        import { createApp } from &#39;vue&#39;;
+        import App from &quot;./App.vue&quot;;
+        import &#39;/node_modules/iuniko-ui/dist/style.css&#39;
+        import Iuniko from &#39;iuniko-ui&#39;;
+
+        const app = createApp(App);
+        app.use(Iuniko);
+        app.mount(&#39;#app&#39;);
+    </code>
+</pre>`,8),c=[r],d={},_="",h=e({__name:"start",setup(a,{expose:o}){return o({frontmatter:{},excerpt:void 0}),(i,m)=>(t(),n("div",s,c))}});export{h as default,_ as excerpt,d as frontmatter};
